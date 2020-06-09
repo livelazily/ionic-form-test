1a401e67b464c40c985ff69a7c607a978d9433f9
livelazily <livelazily@youngfriend.cn>
2020-06-09 16:22:09 +0800

    markAllAsTouched test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() {}
+  constructor(private formBuild: FormBuilder) {}
 
+  ngOnInit() {
+    this.formGroup = this.formBuild.group({
+      field1: ['', Validators.required],
+      field3: ['', Validators.required],
+    });
+  }
+
+  canSubmit() {
+    const form = this.formGroup;
+    if (form.untouched && form.pristine) {
+      return true;
+    }
+    return !form.invalid;
+  }
+
+  submit() {
+    const form = this.formGroup;
+    form.markAllAsTouched();
+
+    if (form.invalid) {
+      return;
+    }
+
+    alert('form submited');
+  }
 }
